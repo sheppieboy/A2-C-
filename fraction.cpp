5a2f6fbc24271439e3d2744fc7ce63bcfb96a298
Luke Sheppard <sheppiegit@gmail.com>
2023-10-18 21:58:40 -0400

    added accessor functions for denominator and numerator .

--- fraction.cpp
+++ fraction.cpp
@@ -10,7 +10,8 @@ int Fraction::gcd(int a, int b) {
     }
     return gcd(b, a % b);
 }
-    
+
+//function to normalzie the 
 void Fraction::normalize(){
     int greatest_common_divisor = gcd(numerator, denominator);
     numerator /= greatest_common_divisor;
@@ -18,12 +19,46 @@ void Fraction::normalize(){
 }
 
 //constructors
-Fraction::Fraction();
+Fraction::Fraction() : numerator(0), denominator(1) {
+}
 
+Fraction::Fraction(int num) : numerator(num), denominator(1) {
+    normalize();
+}
+
+Fraction::Fraction(int num, int denom) : numerator(num), denominator(denom) {
+    if (denominator == 0) {
+        throw FractionException("Denominator cannot be zero.");
+    }
+    normalize();
+}
+
+//accessors
+int Fraction::getNumerator() const {
+    return numerator;
+}
+
+int Fraction::getDenominator() const {
+    return denominator;
+}
 
 //overwrite << operator
 ostream& operator<<(ostream& os, const Fraction& fraction){
-    os << fraction.numerator() << "/" << fraction.denominator();
+    os << fraction.getNumerator() << "/" << fraction.getDenominator();
     return os;
 }
 
+int main() {
+
+	// Test constructors and << operator overloading
+	Fraction test1;
+	cout << "Should be \"0/1\": " << test1 << endl;
+	Fraction test2(4);
+	cout << "Should be \"4/1\": " << test2 << endl;
+	Fraction test3(20, 30);
+	cout << "Should be \"2/3\": " << test3 << endl;
+	Fraction test4(210, -60);
+	cout << "Should be \"-7/2\": " << test4 << endl;
+
+    return 0;
+}
